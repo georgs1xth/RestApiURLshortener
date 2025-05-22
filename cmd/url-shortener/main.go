2b388c04a1656149ed239a9efc4e2e00a4e32d7b
Georg.s1xth <zhorik06bondarenko@gmail.com>
2025-05-22 16:45:40 +0500

    finished storage methods for sqlite3

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,15 +1,69 @@
 // Main package
 package main
 
+import (
+	"fmt"
+	"log/slog"
+	"os"
+	"url-shortener/internal/config"
+	"url-shortener/internal/lib/logger/sl"
+	"url-shortener/internal/storage/sqlite"
+)
+
+const (
+	envLocal = "local"
+	envDev = "dev"
+	envProd = "prod"
+)
+
 func main() {
-	// TODO: init config: cleanenv
+	cfg := config.MustLoad()	
 
-	// TODO: init logger: slog
+	fmt.Println(cfg)
 
-	// TODO: init storage: sqlite3
+	log := setupLogger(cfg.Env)
+	log.Info("starting server", slog.String("env", cfg.Env))
+	log.Debug("debug message are enabled")
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		slog.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	_ = storage
+	
+	err = storage.DeleteURL("google")
+	fmt.Println(err)
+
 
 	// TODO: init router: chi, "chi render"
 
 	// TODO: run server
 
+}
+
+func setupLogger(env string) *slog.Logger{
+
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		}))
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+				Level: slog.LevelDebug,
+		}))
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+				Level: slog.LevelInfo,
+		}))
+	}
+
+	return log
 }
